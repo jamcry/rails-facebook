2486ff7a6884e3f60c226ed8510dfb063ad6928d
jamcry <jamcry@hotmail.com>
2019-06-09 01:30:24 +0300

    Add user avatars via Gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  # Returns the Gravatar (image) of given user
+  def gravatar_for(user, params = { size: 50 })
+    gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{params[:size]}"
+    image_tag(gravatar_url, alt: "#{user.first_name}'s gravatar", class: "gravatar rounded rounded-circle img-fluid d-block")
+  end
 end
