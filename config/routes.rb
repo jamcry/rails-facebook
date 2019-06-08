6ee2087cd52c0135f5cdf7d4fdebf802cfe4123d
jamcry <jamcry@hotmail.com>
2019-06-08 03:18:51 +0300

    Create users controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/show'
   root 'static_pages#index'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
