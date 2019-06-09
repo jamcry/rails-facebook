04b7c0b8c8e255cebc6d7a83956c39d6bca0d606
jamcry <jamcry@hotmail.com>
2019-06-09 03:05:12 +0300

    Implement store_location before action for redirection after actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  #dr112945
+  before_action :store_user_location!, if: :storable_location?
   protected
     # Permit additional parameters for registration form
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :surname, :email, :password, :birthday, :gender_id])
+    end
+
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+    end
+
+    def store_user_location!
+      store_location_for(:user, request.fullpath)
     end
 end
