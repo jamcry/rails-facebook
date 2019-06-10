0a96be1a110a60c88ddef2b1c02e7e85bdab4666
jamcry <jamcry@hotmail.com>
2019-06-10 13:25:54 +0300

    Add descending post view with :feed scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   has_many   :comments, dependent: :destroy
 
   validates :body, presence: true, length: { maximum: 500 }
+
+  scope :feed, -> { order(created_at: :desc) }
 end
