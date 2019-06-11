d594de010ca713d4934f6506b918ef6b25328b47
jamcry <jamcry@hotmail.com>
2019-06-11 15:40:01 +0300

    Add friendship dependence on destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments
-  has_many :friendships
+  has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
