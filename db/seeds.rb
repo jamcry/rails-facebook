699923cea6e4b72761b63ae14686eb3549592f87
jamcry <jamcry@hotmail.com>
2019-06-11 14:38:43 +0300

    Add user,post,comment seeds with 'faker' gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+
+User.create!(first_name: "Cem", surname: "KIRAY", email: "jamcry@hotmail.com", password: "foobar", birthday: Date.new, gender_id: 0)
+
+10.times {
+  u = User.create!(first_name: Faker::Name.first_name,
+                   surname:    Faker::Name.last_name,
+                   email:      Faker::Internet.email,
+                   birthday:   Faker::Date.birthday(18, 50),
+                   password:   "foobar",
+                   gender_id:  rand(0..2))
+  rand(1..35).times {
+    p = u.posts.create!(body: Faker::Quote.famous_last_words)
+    rand(1..15).times {
+      p.comments.create!(body: Faker::Quote.most_interesting_man_in_the_world,
+                         user: User.find(rand((User.first.id)..(User.last.id))))
+    }
+  }
+}
