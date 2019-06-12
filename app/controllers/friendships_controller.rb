f47c21d238f5a013913305583979033ff79a92fe
jamcry <jamcry@hotmail.com>
2019-06-12 18:22:17 +0300

    Add 'pending requests' view to home page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,20 @@
 class FriendshipsController < ApplicationController
+  def create
+    @friend = User.find(params[:friend_id])
+    @friendship = @friend.friendships.build(friend: current_user)
+    @inverse_friendship = current_user.friendships.build(friend: @friend)
+    @request = FriendshipRequest.find_by(user_id: params[:friend_id],
+                                          requested_user_id: current_user.id)
+    if @friendship.save && @inverse_friendship.save
+      flash[:success] = "You are now friends with #{@friend.full_name}!"
+      @request.destroy if @request
+      redirect_to user_path(@friend)
+    else
+      flash[:danger] = "The friendship couldn't be created!"
+      redirect_to stored_location_for(:user)
+    end
+  end
+
   def destroy
     @friendship = Friendship.find(params[:id])
     @inverse_friendship = inverse_friendship(@friendship)
