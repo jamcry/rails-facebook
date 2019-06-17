8f03c47a2628b59cc0660bb322b8f932d822445d
jamcry <jamcry@hotmail.com>
2019-06-17 22:56:08 +0300

    Complete login with facebook feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,4 +68,28 @@ class User < ApplicationRecord
   def send_friendship_request(other_user)
     friendship_requests.create!(requested_user: other_user) if !self.friends_with?(other_user)
   end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      name = auth.info.name.split
+      user.first_name = auth.info.name.split.first
+      user.surname    = auth.info.name.split.last
+      user.email      = auth.info.email
+      user.password   = Devise.friendly_token[0, 20]
+      user.image      = auth.info.image
+      user.gender_id  = 2
+      user.birthday   = Time.new
+      # If user is :confirmable, uncomment the following line
+      # to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
 end
