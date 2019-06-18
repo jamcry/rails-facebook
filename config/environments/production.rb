d36005f4c2346456a218873b6730f5a341ffb8ef
jamcry <jamcry@hotmail.com>
2019-06-18 15:44:33 +0300

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
+  }
   
 
   # Ignore bad email addresses and do not raise email delivery errors.
